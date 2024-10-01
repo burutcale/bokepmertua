dc1ab0b8c7182d89c45ea0dc93f2c32c2e70a352
burutcale <164441639+burutcale@users.noreply.github.com>
2024-10-01 11:08:09 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import CardList from "@/components/card-list";
 import { DEFAULT_PER_PAGE } from "@/lib/constants";
 import SearchCardList from "@/components/search/search-list";
 import { SITENAME } from "@/lib/constants";
-
 export default async function Home({
     searchParams,
 }: {
     searchParams: { [key: string]: string | string[] | undefined };
 }) {
     const page =
-        (searchParams.page && parseInt(searchParams.page as string)) || 18;
+        (searchParams.page && parseInt(searchParams.page as string)) || 1;
     const per_page =
         (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
         DEFAULT_PER_PAGE;
@@ -18,12 +17,13 @@ export default async function Home({
     const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
-        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Barat Terbaru</h2>
+        <div className="md:my-2">
             {query ? (
                 <SearchCardList query={query} banner />
             ) : (
                 <CardList page={page} per_page={per_page} fld_id={fld_id} />
             )}
+		<h1>{SITENAME}</h1><h2>Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru</h2><p>{SITENAME} merupakan situs website hiburan dewasa 18+ nonton video bokep indo yang menyediakan akses ribuan video porno Indonesia terbaru yang bisa anda nikmati secara gratis jadi jangan lupa untuk bookmark ya.</p>
         </div>
     );
 }
